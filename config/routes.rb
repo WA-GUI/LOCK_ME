3f4fda7382629453683e3a6b25c5ff7e81f26b45
Georgi Mihov <georgi.mihov.06@gmail.com>
2022-06-06 17:27:48 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+
+  # root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   root 'users#index'
+
+  resources :users do
+    resources :booking, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
