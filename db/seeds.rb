1701e86a57afb6702f2ce246a68036d2da1e3599
Guillaume Genet <wagui.genet@gmail.com>
2022-06-08 13:13:51 +0200

    MAJ de la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 Service.destroy_all
 Service.create(name: "Cleaning", price: 100)
 Service.create(name: "Cooking", price: 100)
-Service.create(name: "Call Boy", price: 300)
+# Service.create(name: "Call Boy", price: 300)
 Service.create(name: "Body Sushis", price: 100)
 Service.create(name: "Dance", price: 200)
 Service.create(name: "Coach Sportif", price: 250)
 Service.create(name: "Body Massage", price: 150)
-Service.create(name: "Full Service", price: 500)
+# Service.create(name: "Full Service", price: 500)
 puts "#{Service.count} services created"
